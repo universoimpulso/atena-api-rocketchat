da84a3d6d47e0c5b34cf010df15090b1d98aefa9
Eduardo Junior <goldblade@gmail.com>
2019-01-08 10:44:34 -0300

    first commit

---
+++ index.ts
@@ -0,0 +1,30 @@
+import {
+    IConfigurationExtend,
+    ILogger,
+} from '@rocket.chat/apps-engine/definition/accessors';
+import { App } from '@rocket.chat/apps-engine/definition/App';
+import { IAppInfo } from '@rocket.chat/apps-engine/definition/metadata';
+import { SettingType } from '@rocket.chat/apps-engine/definition/settings';
+import { AtenaRankingSlashCommand, AtenaSlashCommand } from './slashcommand';
+
+export class AtenaApiRocketChatApp extends App {
+    constructor(info: IAppInfo, logger: ILogger) {
+        super(info, logger);
+    }
+
+    protected async extendConfiguration(configuration: IConfigurationExtend): Promise<void> {
+	    this.getLogger().log('Hellow world from my app');
+      await configuration.slashCommands.provideSlashCommand(new AtenaSlashCommand(this));
+      await configuration.slashCommands.provideSlashCommand(new AtenaRankingSlashCommand(this));
+
+      await configuration.settings.provideSetting({
+          id: 'server',
+          type: SettingType.STRING,
+          packageValue: 'https://atena-goldblade.serveo.net/',
+          required: true,
+          public: false,
+          i18nLabel: 'server',
+          i18nDescription: 'server_description',
+        });
+    }
+}
