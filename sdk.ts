e95b22338d0ac239a84c7a28ed5b1d291c24279f
Eduardo Junior <goldblade@gmail.com>
2019-01-11 08:45:40 -0300

    add slash command

---
+++ sdk.ts
@@ -0,0 +1,40 @@
+import { IHttp, IHttpRequest, IRead } from '@rocket.chat/apps-engine/definition/accessors';
+import { IUser } from '@rocket.chat/apps-engine/definition/users';
+
+//export interface IVerification {
+//  algo: string;
+//}
+
+//export interface IAtenaResponse<T> {
+//  result: Array<T>;
+//  succcess: boolean;
+//}
+
+// errors: Array<any>;
+// messages: Array<any>;
+
+class SDK {
+  public async getRanking(http: IHttp, read: IRead, sender: IUser, month: string) {
+    const server = await read.getEnvironmentReader().getSettings().getValueById('server');
+    const url = `${server}bot/commands/ranking`;
+    const param = (month ? month : "");
+    const data = {
+      ... await sender,
+      month: param
+    };
+    const options: IHttpRequest = {
+      data: data,
+      headers: {
+        "Content-Type": "application/json",
+        "origin": "rocket"
+      }
+    };
+    const response = await http.post(url, options);
+
+    // return response.data as IAtenaResponse<IVerification>;
+    return response.data;
+  }
+}
+
+
+export const sdk = new SDK();
